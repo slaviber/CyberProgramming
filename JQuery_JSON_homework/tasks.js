3360f633f6cfedb82826f6bc90376799f22d1d2e
slaviber <slavimber@gmail.com>
2014-12-31 04:08:06 +0200

    task4

--- JQuery_JSON_homework/tasks.js
+++ JQuery_JSON_homework/tasks.js
@@ -4,5 +4,7 @@
  	console.log($('#footer a.tu').attr("title"));
  	
  	console.log($('#home #col1 p').text());
+ 	
+ 	$('#header #menu-top-level-menu').append('<li> <a href = "#"> new button ');
  
  });
